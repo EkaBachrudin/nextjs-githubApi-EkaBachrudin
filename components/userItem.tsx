6b95cb0a5867e148eae074c32e92d79480aceefa
EkaBachrudin <vvalentine825@gmail.com>
2023-01-26 00:29:10 +0700

    Rounded Image Style

--- components/userItem.tsx
+++ components/userItem.tsx
@@ -8,7 +8,7 @@ export default function UserItem({user}:Props) {
   return (
     <div className='d-flex justify-content-between'>
         <div className='my-2'>
-           <Image alt="profile" width={40} height={40} src={user?.avatar_url}/>
+           <Image alt="profile" className='rounded-circle shadow' width={40} height={40} src={user?.avatar_url}/>
            <span className='ms-5'>{user.login}</span>
         </div>
         <span className='d-flex align-self-center align-items-center'><Link className='button ms-2 shadow-sm text-white' href={`/detail/${user.login}`}>   <RightIcon/>   </Link></span>
